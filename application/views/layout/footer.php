1f507a50461a135c2a987633ebb7cc081e732f0c
= <vui.trinhtk@gmail.com>
2020-11-29 21:26:56 +0700

    Giao dien 29/11/20

---
+++ application/views/layout/footer.php
@@ -0,0 +1,37 @@
+	<div class="container-fluid">
+		<div class="col-xs-12 col-sm-12 col-md-4 mb20">
+			<h4>Danh sách sinh viên</h4>
+			<div class="content">
+				<li>1851010034 - Đỗ Trọng Đức</li>
+				<li>1851010157 - Trịnh Thị Kim Vui</li>
+				<li>1851010104 - Nguyễn Hoàng Phúc</li>
+			</div>
+		</div>
+	</div>
+	<div class="col-xs-12 col-sm-6 col-md-2 mb20">
+		<h4>Liên kết nhanh</h4>
+		<ul class="footer-menu">
+			
+			<li><a href="/blogs/html">HTML</a></li>
+			
+			<li><a href="/blogs/css">CSS</a></li>
+			
+			<li><a href="/blogs/javascript">Javascript</a></li>
+			
+			<li><a href="/blogs/jquery">Jquery</a></li>
+			
+			<li><a href="/blogs/css-tricks">CSS Tricks</a></li>
+			
+			<li><a href="/pages/chrome-extensions-cho-front-end-developer">Thủ thuật</a></li>
+			
+			<li><a href="/blogs/videos">Videos</a></li>
+			
+		</ul>
+		</div>
+		<div class="jumbotron text-center" style="margin-bottom:0">
+			<p>
+			@Copright - 2020
+			</p>
+		</div>
+	</body>
+</html>
---
+++ application/views/layout/sidebar.php
@@ -0,0 +1,26 @@
+<div class="col-sm-4">
+	<div class="sidebar-left">
+	   	<h3 class="sidebar-title">Danh mục</h3>
+	   	<ul class="sidebar-menu">
+	    	<li><a href="#" >Trang chủ</a> </li>
+	    	<li class="has-child">
+		        <a href="#" >Tin tức<i class="icon"></i></a> 
+		        <ul class="menu-sub" >
+		           	<li><a href="#" >Thế giới</a> </li>
+		            <li><a href="#" >Thể thao</a> </li>
+			        <li><a href="#" >Giáo dục</a> </li>
+			        <li><a href="#" >Du lịch</a> </li>
+	        	</ul>
+	      	</li>
+	      	<li class="has-child">
+		        <a href="#" >Về chúng tôi <i class="icon"></i></a> 
+		        <ul class="menu-sub" >
+		            <li><a href="#" >1851010034 - Đỗ Trọng Đức</a> </li>
+		            <li><a href="#" >1851010157 - Trịnh Thị Kim Vui</a> </li>
+		            <li><a href="#" >1851010104 - Nguyễn Hoàng Phúc</a> </li>
+		        </ul>
+	       	</li>
+	       	<li><a href="#" >Tổng số lượt truy cập</a> </li>
+	    </ul>
+	</div>
+</div>
